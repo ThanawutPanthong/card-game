e401256e11312d24e48f122bfa2fccf1b8a9bc35
Dreamzaa <daremzaa555@gmail.com>
2019-09-21 16:53:38 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg';
+import WordCard from './WordCard';
+const word = ['Hello','React'];
+var item = word[Math.floor(Math.random()*word.length)];
+class App extends Component {
+ render() {
+ return (
+ < div className = "App">
+   <h2 id="layer">Random Game</h2>
+    <WordCard value = {item.toUpperCase()}/>
+    <h2 id ="results"></h2>
+ </div>
+ );
+ }
 }
-
 export default App;
+
---
+++ src/WordCard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+ let word = given_word.toUpperCase()
+ let chars = _.shuffle(Array.from(word))
+ return {
+ word,
+ chars,
+ attempt: 1,
+ guess: [],
+ completed: false
+ }
+}
+export default class WordCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+
+    activationHandler = (c) =>{
+        let guess = [...this.state.guess, c.toUpperCase()]
+        this.setState({guess})
+
+      
+        if(guess.length == this.state.chars.length){
+            console.log(guess.join('').toString())
+            console.log(this.state.chars.join('').toString())
+            if(guess.join('').toString() == this.state.chars.join('').toString()){
+                this.setState({guess: [], complete: true})
+                document.getElementById('results').innerHTML = `Congratulations!`
+                
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+                document.getElementById('results').innerHTML = `Attempt : ${this.state.attempt} `
+            }
+        }
+    }
+       
+render() {
+ return (
+ <div>
+{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}{...this.state}/>
+) }
+ </div>
+ );
+ }
+}
